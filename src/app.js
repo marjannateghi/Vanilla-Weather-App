c77e12cc618cd6a41b682580d4d8c53d1d5a99d7
marjannateghi <134637909+marjannateghi@users.noreply.github.com>
2023-06-12 00:29:28 +0330

    update js date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function showTemperature(response) {
-    console.log(response.data);
+
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     
@@ -13,10 +41,14 @@ function showTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
 
     let windElement = document.querySelector("#wind");
-    windElement.innerHTML = response.data.wind.speed;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.date.dt * 1000);
 }
 
 let apiKey = "233f4519d2bc22454f1e109671d01c65";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`;
+let city = "Tehran";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
