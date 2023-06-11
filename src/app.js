531be05db79a25a6dc9da07de6bf27c10102fe93
marjannateghi <134637909+marjannateghi@users.noreply.github.com>
2023-06-11 21:31:03 +0330

    some other js added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 function showTemperature(response) {
-    console.log(response.data.main.temp);
+    console.log(response.data.weather[0].description);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement = response.data.weather[0].description;
 }
 
 let apiKey = "233f4519d2bc22454f1e109671d01c65";
